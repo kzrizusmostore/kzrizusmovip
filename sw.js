6f5cbdf41a379b3a9333a7b685ab92f52d37ff26
kzrizusmo <kzrizusmostore@gmail.com>
2026-09-26 07:35:25 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'kzrizusmo-ff-pwa-v1';
-const APP_SHELL = ['/', '/manifest.webmanifest'];
+const CACHE_NAME = 'kzrizusmo-ff-pwa-v4';
+const APP_SHELL = ['/', '/manifest.webmanifest', '/kz-icon-192-v2.png', '/kz-icon-512-v2.png'];
 
 self.addEventListener('install', event => {
   self.skipWaiting();
