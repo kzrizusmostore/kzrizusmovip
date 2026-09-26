ff0b1ae7df4060426377cec0bc876e65ee35b087
kzrizusmo <kzrizusmostore@gmail.com>
2026-09-26 07:46:44 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,21 @@
-const CACHE_NAME = 'kzrizusmo-ff-pwa-v4';
-const APP_SHELL = ['/', '/manifest.webmanifest', '/kz-icon-192-v2.png', '/kz-icon-512-v2.png'];
-
-self.addEventListener('install', event => {
+const CACHE_NAME='kzrizusmo-ff-pwa-v5';
+const STATIC_ASSETS=[
+  '/manifest.webmanifest',
+  '/kz-icon-192-v2.png',
+  '/kz-icon-512-v2.png'
+];
+self.addEventListener('install',event=>{
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).catch(() => undefined));
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(STATIC_ASSETS)).catch(()=>undefined));
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put('/', copy)).catch(() => {});
-        return response;
-      }).catch(() => caches.match('/'))
-    );
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match('/')));
     return;
   }
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-      if (response && response.status === 200 && response.type === 'basic') {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
-      }
-      return response;
-    }))
-  );
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
 });
